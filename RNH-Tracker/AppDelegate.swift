ffff464f64e326704a272e083f240e7a87cc7fbd
Ed Cafferatra <cafferata@live.nl>
2021-08-27 19:46:05 +0200

    .

--- RNH-Tracker/AppDelegate.swift
+++ RNH-Tracker/AppDelegate.swift
@@ -1,10 +1,5 @@
-//
 //  AppDelegate.swift
-//  OpenGpxTracker
-//
-//  Created by merlos on 13/09/14.
-//  Copyright (c) 2014 TransitBox. All rights reserved.
-//
+//  RNH-Tracker
 
 import UIKit
 import CoreData
